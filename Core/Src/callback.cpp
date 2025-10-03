fbac518b6e63f59a60585f357617a5a1cb2326e7
zhuxinyue <zhu-xin-yue@sjtu.edu.cn>
2025-10-03 15:50:15 +0800

    chore:添加.clang-tidy

--- Core/Src/callback.cpp
+++ Core/Src/callback.cpp
@@ -13,21 +13,16 @@ extern uint8_t tx_data[8];
 extern uint8_t rx_data[8];
 M3508_Motor Motor(19.2f);
 
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
-    if (htim -> Instance == htim6.Instance)
-    {
-        HAL_CAN_AddTxMessage(&hcan1,&tx_header,tx_data,&can_tx_mail_box_);
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim) {
+    if (htim->Instance == htim6.Instance) {
+        HAL_CAN_AddTxMessage(&hcan1, &tx_header, tx_data, &can_tx_mail_box_);
     }
 }
 
-void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
-{
-    if (hcan->Instance == CAN1)
-    {
-        HAL_CAN_GetRxMessage(&hcan1,CAN_RX_FIFO0,&rx_header,rx_data);
-        if (rx_header.StdId == 0x201)
-        {
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef* hcan) {
+    if (hcan->Instance == CAN1) {
+        HAL_CAN_GetRxMessage(&hcan1,CAN_RX_FIFO0, &rx_header, rx_data);
+        if (rx_header.StdId == 0x201) {
             Motor.canRxMsgCallback(rx_data);
         }
     }
